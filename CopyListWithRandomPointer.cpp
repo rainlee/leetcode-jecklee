99d330c80fd9dd860add7f299d64f009c91b5996
jecklee <leeht2008@gmail.com>
2014-07-04 12:06:38 +0800

    add a more elegant solution

--- CopyListWithRandomPointer.cpp
+++ CopyListWithRandomPointer.cpp
@@ -11,6 +11,7 @@
  * 第一次复制所有的next节点，并且保存新旧节点的map
  * 第二次在map中查找random对应的新值，并更新random指针
  ***/
+/*
 class Solution {
 public:
     RandomListNode *copyRandomList(RandomListNode *head) {
@@ -50,5 +51,52 @@ public:
         }
         
         return phead2->next;
+    }
+};
+*/
+
+/***
+ * 法2：法1中的map是为了保存新旧节点的映射关系
+ * 可以直接在原链表的每个节点后复制一份 1->2->3->NULL ==> 1->1->2->2->3->3->NULL
+ * 然后遍历第二遍，复制random节点，对应新节点的random即为random->next
+ * 最后，将链表拆成两个
+ ***/
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        if (NULL == head)
+            return NULL;
+        
+        RandomListNode *node = head;   // copy node
+        while (node)
+        {
+            RandomListNode *newnode = new RandomListNode(node->label);
+            newnode->next = node->next;
+            node->next = newnode;
+            
+            node = newnode->next;
+        }
+        
+        node = head;   // copy random node
+        while (node)
+        {
+            if (node->random != NULL)   // caution: maybe NULL
+                node->next->random = node->random->next;
+            node = node->next->next;
+        }
+        
+        node = head;
+        RandomListNode *prehead2 = new RandomListNode(-1);  // partition
+        RandomListNode *node2 = prehead2;
+        while (node)
+        {
+            node2->next = node->next;
+            node2 = node->next;
+            
+            node->next = node->next->next;
+            node = node->next;
+        }
+        
+        return prehead2->next;
     }
 };
