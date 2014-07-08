8b87dd2f07d19faf79ab5630ef4682abc6591ee6
rainlee <leeht2008@gmail.com>
2014-07-08 15:53:07 +0800

    Wildcard match

---
+++ RegularExpressionMatching.cpp
@@ -0,0 +1,88 @@
+/***
+ * 递归 + 回溯
+ * 由于'*'和前一个元素相关，所以每次判断后一个元素*(p+1)
+ * 完全匹配的条件是s p 都到达末尾
+ * 若*(p+1) != '*'，则前一个必须匹配（相等或者有'.'）
+ *          == '*'，若前一个不匹配，则比较(s, p+2) 跳过p和p+1
+ *                            匹配，则比较(s+1, p) 跳过s || (s, p+2) 跳过'*'
+ * 注意：'*'匹配前一个元素的0个或多个
+ ***/
+/*
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if ('\0' == *p)   // the end
+            return ('\0' == *s);
+        
+        if (*(p+1) != '*')
+        {
+            if (('\0' != *s) && ((*s == *p) || (*p == '.')))
+                return isMatch(s+1, p+1);
+            else
+                return false;
+        }
+        else
+        {
+            if (('\0' != *s) && ((*s == *p) || (*p == '.')))
+                return isMatch(s+1, p) || isMatch(s, p+2);    // use '*' || jump '*'
+            else
+                return isMatch(s, p+2);  // jump '*'
+        }
+    }
+};
+*/
+
+/***
+ * 法2：在法1的基础上 另一种递归
+ * 当*(p+1) == '*'，此时看从s[i]开始的子串
+ * 假设s[i],s[i+1],...s[i+k]都等于p[j]那么意味着这些都有可能是合适的匹配
+ * 那么递归对于剩下的(i,j+2),(i+1,j+2),...,(i+k,j+2)都要尝试（j+2是因为跳过当前和下一个'*'字符）。 
+ ***/
+/*
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if ('\0' == *p)
+            return ('\0' == *s);
+        
+        if ('*' != *(p+1))
+        {
+            if (('\0' != *s) && ((*s == *p) || (*p == '.')))
+                return isMatch(s+1, p+1);
+            else
+                return false;
+        }
+        else
+        {
+            while (('\0' != *s) && ((*s == *p) || (*p == '.')))
+            {
+                if (isMatch(s, p+2))  // jump '*'
+                    return true;
+                ++s;                  // next s
+            }
+            return isMatch(s, p+2);   // pre not match, have to jump '*'
+        }
+    }
+};
+*/
+
+/***
+ * 法3：另外一种递归逻辑，判断当前元素*p（法1是先判断*(p+1)）
+ * 若*p == *s，若*(p+1) != '*'，直接后移，比较(s+1, p+1)
+ *                      ==      使用'*'通配 (s+1, p) || 不使用'*' (s, p+2)
+ *      !=              !=      false
+ *                      ==      跳过 比较(s, p+2)
+ ***/
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if ('\0' == *p)
+            return ('\0' == *s);
+        
+        if (('\0' != *s) && ((*p == *s) || ('.' == *p)))
+            return (*(p+1) == '*') ? (isMatch(s+1, p) || isMatch(s, p+2)) : 
+                                      isMatch(s+1, p+1);
+        else
+            return (*(p+1) == '*') ? isMatch(s, p+2) : false;
+    }
+};
---
+++ WildcardMatching.cpp
@@ -0,0 +1,70 @@
+/***
+ * 递归，同Regular Expression Matching 
+ * 注意这里的'*'是通配符，和前一个元素无关
+ * 递归超时……
+ ***/
+/*
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        if ('\0' == *p)
+            return ('\0' == *s);
+        if ('\0' == *s)
+            return ('*' == *p) && ('\0' == *(p+1));
+        
+        if (*p == '*')
+        {
+            while (*p == '*') ++p;   // skip continous '*'
+            return isMatch(s+1, p-1) || isMatch(s, p);  // use '*' || jump '*'
+        }
+        else if (('\0' != *s) && ((*p == *s) || ('?' == *p)))
+            return isMatch(s+1, p+1);
+        else
+            return false;
+    }
+};
+*/
+
+/***
+ * 法2：迭代 + 回溯
+ * 匹配*s *p，若匹配，则++s ++p
+ * 若不匹配，则检查p之前是否有'*'
+ * 是否有*需要记录,遇到*就记录当前*的位置和匹配到的s的位置
+ * 然后从*的下一位置和记录的s的下一位置匹配（回溯）
+ ***/
+class Solution {
+public:
+    bool isMatch(const char *s, const char *p) {
+        const char *sbackup = NULL;
+        const char *pbackup = NULL;
+        while (*s != '\0')
+        {
+            if ((*p == '?') || (*p == *s))  // match
+            {
+                ++s;
+                ++p;
+            }
+            else if (*p == '*')  // mark the pos of '*'
+            {
+                while (*p == '*')  // skip continous '*'
+                    ++p;
+                if (*p == '\0')    // end with '*'
+                    return true;
+                
+                sbackup = s;
+                pbackup = p;
+            }
+            else   // not match, back up
+            {
+                if (!sbackup)
+                    return false;
+                
+                s = ++sbackup;   // '*' matches (*sbackup), move to next s
+                p = pbackup;
+            }
+        }
+        while (*p == '*')  // skip '*'
+            ++p;
+        return (*p == '\0');
+    }
+};
