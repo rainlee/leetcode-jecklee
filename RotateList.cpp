56965a5c3a387ea9a3b18047adb82033cb331245
rainlee <leeht2008@gmail.com>
2014-07-29 19:39:09 +0800

    add a solution

--- RotateList.cpp
+++ RotateList.cpp
@@ -13,6 +13,7 @@
  * 快指针到达尾部时，慢指针指在倒数第k个
  * note：k是否会超过表长
  ***/
+/*
 class Solution {
 public:
     ListNode *rotateRight(ListNode *head, int k) {
@@ -39,6 +40,36 @@ public:
         head = pslow->next;  // rotate list
         pslow->next = NULL;
         
+        return head;
+    }
+};
+*/
+
+/***
+ * 法2：先遍历一遍，得出链表长度len，注意k可能大于len，因此令k%=len。
+ * 将尾节点next指针指向首节点，形成一个环，接着往后跑len k步，从这里断开，就是要求的结果了。
+ ***/
+class Solution {
+public:
+    ListNode *rotateRight(ListNode *head, int k) {
+        if ((NULL == head) || (0 == k))
+            return head;
+        
+        int len = 1;
+        ListNode *node = head;
+        while (node->next)
+        {
+            ++len;
+            node = node->next;
+        }
+        node->next = head;   // endless list
+        
+        int step = len - (k % len);
+        while (step--)
+            node = node->next;
+        
+        head = node->next;  // new head
+        node->next = NULL;  // break the list
         return head;
     }
 };
