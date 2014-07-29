21abdc1c43e2299c5b5927f5de2a257f1f31c5c3
rainlee <leeht2008@gmail.com>
2014-07-29 20:15:50 +0800

    add a solution

--- SearchA2DMatrix.cpp
+++ SearchA2DMatrix.cpp
@@ -3,6 +3,7 @@
  * == target, 找到
  * < target, 去掉最右一列
  * > target, 去掉最上面一行
+ * 时间复杂度 O(m+n)
  ***/
 /*
 class Solution {
@@ -35,7 +36,9 @@ public:
  * 法2：同样行列扫描，分段查找，二分查找
  * 先对列进行二分，找到在段（行）
  * 再对行进行二分，找到target
+ * 时间复杂度 O(logn + logm)
  ***/
+/*
 class Solution {
 public:
     bool searchMatrix(vector<vector<int> > &matrix, int target) {
@@ -77,6 +80,41 @@ public:
                 low = j + 1;
             else
                 high = j - 1;
+        }
+        return false;
+    }
+};
+*/
+
+/***
+ * 法3：一次二分查找
+ * 将矩阵展开成一维后是个有序数组，直接对其进行查找
+ * low = 0, high = m*n - 1, mid = (low+high)/2
+ * 将一维的mid转换成二维的横纵坐标
+ * y = mid / n, x = mid % n
+ * 时间复杂度O(m*n)
+ ***/
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int> > &matrix, int target) {
+        if (matrix.empty() || matrix[0].empty())
+            return false;
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        int low = 0;
+        int high = m*n - 1;
+        while (low <= high)
+        {
+            int mid = (low & high) + ((low ^ high) >> 1);
+            int y = mid / n;   // row
+            int x = mid % n;   // col
+            if (target == matrix[y][x])
+                return true;
+            else if (target > matrix[y][x])
+                low = mid + 1;
+            else
+                high = mid - 1;
         }
         return false;
     }
