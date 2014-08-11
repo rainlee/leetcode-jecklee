cae2f8a30fd9b217a5482684b5c4184fc253b595
rainlee <leeht2008@gmail.com>
2014-08-11 10:01:46 +0800

    add a new solution to DFS

--- SubsetsII.cpp
+++ SubsetsII.cpp
@@ -4,6 +4,7 @@
  * 当前数和前一个相等，若前一个没选，当前这个也不能选
  * 又忘了排序……
  ***/
+/*
 class Solution {
 public:
     vector<vector<int> > subsetsWithDup(vector<int> &S) {
@@ -37,6 +38,40 @@ private:
         
         dfs(S, deep+1, path);
         
+        return;
+    }
+};
+*/
+
+/***
+ * 法2：另一种递归方法
+ * 注意：1 去重, 2 处理不选的情况
+ ***/
+class Solution {
+public:
+    vector<vector<int> > subsets(vector<int> &S) {
+        sort(S.begin(), S.end());
+        vector<int> path;
+        subsRec(S, 0, path);
+        return vsets;
+    }
+private:
+    vector<vector<int> > vsets;
+    
+    void subsRec(vector<int> &s, int start, vector<int> &path)
+    {
+        if (start == s.size())
+        {
+            vsets.push_back(path);
+            return;
+        }
+        for (int i = start; i < s.size(); ++i)
+        {
+            path.push_back(s[i]);
+            subsRec(s, i+1, path);
+            path.pop_back();
+        }
+        subsRec(s, s.size(), path);   // choose nothing in this level
         return;
     }
 };
