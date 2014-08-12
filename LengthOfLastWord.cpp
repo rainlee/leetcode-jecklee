7640e0e31caaf2551522c424be0cbe30ac5be68a
rainlee <leeht2008@gmail.com>
2014-08-12 10:00:15 +0800

    add a solution

--- LengthOfLastWord.cpp
+++ LengthOfLastWord.cpp
@@ -1,6 +1,7 @@
 /***
  * 顺序扫描，记录每个word的长度
  ***/
+/*
 class Solution {
 public:
     int lengthOfLastWord(const char *s) {
@@ -13,5 +14,30 @@ public:
                 len = 0;
         }
         return len;
+    }
+};
+*/
+
+/***
+ * 注意开头和结尾的空格
+ * 用last记录上一个有效字符长度
+ ***/
+class Solution {
+public:
+    int lengthOfLastWord(const char *s) {
+        int last = 0;
+        int cnt = 0;
+        while ('\0' != *s)
+        {
+            if (' ' == *s)
+            {
+                last = (cnt ? cnt : last);
+                cnt = 0;
+            }
+            else
+                ++cnt;
+            ++s;
+        }
+        return (cnt ? cnt : last);
     }
 };
