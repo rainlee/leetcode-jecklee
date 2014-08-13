10ae98102f90f9e9019a9c43f6741b6da2790abe
rainlee <leeht2008@gmail.com>
2014-08-14 00:38:21 +0800

    bit op

--- AddBinary.cpp
+++ AddBinary.cpp
@@ -8,17 +8,15 @@ public:
         int i = a.size() - 1;
         int j = b.size() - 1;
         int c = 0;   // carry
-        while ((i >= 0) || (j >= 0))
+        while ((i >= 0) || (j >= 0) || c)
         {
             int numa = (i >= 0) ? (a[i--] - '0') : 0;
             int numb = (j >= 0) ? (b[j--] - '0') : 0;
             int num = numa + numb + c;
-            c = num / 2;
-            num %= 2;
+            c = num >> 1;
+            num &= 0x1;
             sum = (char)(num + '0') + sum;  // low digit in low bit
         }
-        if (c)
-            sum = '1' + sum;
         return sum;
     }
 };
