8d6c4ca5be19830eac2d75a65525e1bfc78336d0
rainlee <leeht2008@gmail.com>
2014-08-21 17:18:47 +0800

    add a new solution, use two pointers

--- RemoveElement.cpp
+++ RemoveElement.cpp
@@ -2,6 +2,7 @@
  * 从前往后遍历，并复制
  * 相等则不复制
  ***/
+/*
 class Solution {
 public:
     int removeElement(int A[], int n, int elem) {
@@ -10,5 +11,36 @@ public:
             if (A[i] != elem)
                 A[len++] = A[i];
         return len;
+    }
+};
+*/
+
+/***
+ * 法2：双指针 夹逼
+ * low指向第一个等于elem的元素，high指向第一个不等于elem的元素
+ * 然后两者交换（题目不要求保持原顺序）
+ * 相比法1 减少了复制次数（代码复杂了很多……）
+ * 注意：最后结束时low==high可能都指向elem
+ ***/
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+        if (0 == n)
+            return 0;
+        int low = 0;
+        int high = n-1;
+        while (low < high)
+        {
+            while ((low < high) && (A[low] != elem))
+                ++low;
+            while ((low < high) && (A[high] == elem))
+                --high;
+            if (low < high)
+                swap(A[low], A[high]);
+        }
+        if (A[low] == elem)
+            return low;
+        else
+            return low+1;
     }
 };
