8c406d13c8d1e43c1b487c95ea15801adaea16af
rainlee <leeht2008@gmail.com>
2014-08-21 15:17:52 +0800

    ASCII, hash array 256

--- LongestSubstringWithoutRepeatingCharacters.cpp
+++ LongestSubstringWithoutRepeatingCharacters.cpp
@@ -5,6 +5,7 @@
  * 如果该字符已经出现，则计算当前窗口的长度，并更新字符位置，滑动窗口
  * 记录最长的窗口长度
  ***/
+/*
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
@@ -30,5 +31,30 @@ public:
         maxw = max(maxw, (int)(s.size() - wstart));   // caution: the last seg!!
         
         return maxw;
+    }
+};
+*/
+
+/***
+ * 法2：如果是ASCII码值，直接开一个长为256的数组
+ ***/
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> vmap(256, -1);  // index
+        int maxl = 0;
+        int wstart = 0;
+        for (int i = 0; i < s.size(); ++i)
+        {
+            if (vmap[s[i]] >= wstart)
+            {
+                int window = i - wstart;
+                wstart = vmap[s[i]] + 1;
+                maxl = max(maxl, window);
+            }
+            vmap[s[i]] = i;
+        }
+        maxl = max(maxl, (int)(s.size() - wstart));   // caution: the last seg!!
+        return maxl;
     }
 };
