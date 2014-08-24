ff5e8d69baea56c89203d3bc746a3db5815bc50b
rainlee <leeht2008@gmail.com>
2014-08-24 19:34:52 +0800

    more practice

--- ReverseLinkedListII.cpp
+++ ReverseLinkedListII.cpp
@@ -1,3 +1,25 @@
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         God Bless Me     BUG Free Forever
+*/
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,6 +31,7 @@
 /***
  * 计数 + 就地逆置
  * 先找到m点，然后m-n逆置，最后把前中三段拼起来
+ * 把m-n前插到tail1的后面，注意中间这段不要和后面断开了
  ***/
 class Solution {
 public:
@@ -47,3 +70,41 @@ public:
         return phead->next;
     }
 };
+
+/*
+// 链表的题很容易出错
+// 这种方法是只管前插，中间这段前插完了，再用head2（这时候在链尾）和后面连起来
+class Solution {
+public:
+    ListNode *reverseBetween(ListNode *head, int m, int n) {
+        if (!head || !(head->next))
+            return head;
+        
+        ListNode dummy(-1);
+        dummy.next = head;
+        ListNode *node = &dummy;
+        int cnt = 1;
+        while (node && (cnt < m))
+        {
+            ++cnt;
+            node = node->next;
+        }
+        ListNode *tail = node;  // tail of first part
+        ListNode *head2 = node->next;
+        node = head2->next;
+        while (node && (cnt < n))
+        {
+            ++cnt;
+            ListNode *tailnext = tail->next;
+            ListNode *nodenext = node->next;
+            
+            tail->next = node;
+            node->next = tailnext;
+            
+            node = nodenext;
+        }
+        head2->next = node;
+        return dummy.next;
+    }
+};
+*/
