8b1a9d05a988b6ef10b454a19d9bd561dcb53eff
rainlee <leeht2008@gmail.com>
2014-09-02 09:09:50 +0800

    add new solution

--- BinaryTreeLevelOrderTraversalII.cpp
+++ BinaryTreeLevelOrderTraversalII.cpp
@@ -1,3 +1,25 @@
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         God Bless Me     BUG Free Forever
+*/
 /**
  * Definition for binary tree
  * struct TreeNode {
@@ -14,6 +36,7 @@
  * 每一层的遍历结果往前插就行了
  * 注：vector insert 耗时152ms，而先push_back，最后再调整顺序，只需要20ms!?
  ***/
+/*
 class Solution {
 public:
     vector<vector<int> > levelOrderBottom(TreeNode *root) {
@@ -65,4 +88,48 @@ public:
     }
 private:
     vector<vector<int> > vvtree;
+};
+*/
+
+/***
+ * 法2：用NULL标识一行的结尾
+ ***/
+class Solution {
+public:
+    vector<vector<int> > levelOrderBottom(TreeNode *root) {
+        vector<vector<int> > vtree;
+        if (!root)
+            return vtree;
+        
+        queue<TreeNode *> qtree;
+        qtree.push(root);
+        qtree.push(NULL);
+        vector<int> vlevel;
+        while (true)
+        {
+            TreeNode *node = qtree.front();
+            qtree.pop();
+            if (!node)  // end of level
+            {
+                vtree.push_back(vlevel);
+                vlevel.clear();
+                if (qtree.empty())
+                    break;
+                qtree.push(NULL);
+            }
+            else
+            {
+                vlevel.push_back(node->val);
+                if (node->left)
+                    qtree.push(node->left);
+                if (node->right)
+                    qtree.push(node->right);
+            }
+        }
+        // reverse vector
+        //for (int i = 0; i < vtree.size()/2; ++i)
+        //    vtree[i].swap(vtree[vtree.size() - i - 1]);
+        reverse(vtree.begin(), vtree.end());
+        return vtree;
+    }
 };
